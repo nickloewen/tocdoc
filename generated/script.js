f63adc9afe84884a93a6cf6d6c543f00613043c6
Nick Loewen <nick@nloewen.com>
2020-09-30 13:55:42 -0700

    Update generated example

--- generated/script.js
+++ generated/script.js
@@ -14,7 +14,8 @@ function showSidebar() {
 }
 
 sidebarToggle.addEventListener(
-  'click', () => {
+  'click', (e) => {
+    e.stopPropagation();
     if (sidebar.className === "hidden") {
       showSidebar();
     } else {
@@ -26,13 +27,32 @@ sidebarToggle.addEventListener(
 function autohideSidebar() {
   if (window.innerWidth < 800) {
     hideSidebar();
-    main.addEventListener('click', hideSidebar);
   }
 }
 
+autohideSidebar();
 window.addEventListener('resize', autohideSidebar);
 
-autohideSidebar();
+// For mobile devices/small screens:
+// Make clicking anywhere on the sidebar show it,
+// and anywhere on the doucment hide it
+function narrowScreenSidebarToggle() {
+  // It might be better not to run these every time
+  // the window changes size, but since resizes are
+  // a pretty rare event it doesn't seem worth
+  // complicating this logic.
+  if (window.innerWidth < 600) {
+    main.addEventListener('click', hideSidebar);
+    sidebar.addEventListener('click', showSidebar);
+  } else { // Wide window
+    main.removeEventListener('click', hideSidebar);
+    sidebar.removeEventListener('click', showSidebar);
+  }
+}
+
+narrowScreenSidebarToggle();
+window.addEventListener('resize', narrowScreenSidebarToggle);
+
 
 // Remove "no-animations" after page has loaded
 setTimeout( () => { document.body.className=""; },500);
